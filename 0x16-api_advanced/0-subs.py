236ea7a049f1b8061f4828454dd89506ec524cab
laurendobratz00 <laurendobratz@icloud.com>
2021-05-19 19:01:02 +0000

    add file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,15 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ returns number of subscribers """
-    if not subreddit:
+    url = "http://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {
+        'User-Agent': 'nugget'
+    }
+    x = requests.get(url, headers=headers)
+    if not x:
         return 0
     else:
-        url = "http://api.reddit.com/r/{}/about".format(subreddit)
-        headers = {
-            'User-Agent': 'nugget'
-            }
-        x = requests.get(url, headers=headers)
         data = x.json()
         count = data.get('data').get('subscribers')
         return count
+        
