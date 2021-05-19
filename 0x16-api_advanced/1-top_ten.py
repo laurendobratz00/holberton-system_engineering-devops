59c6cf80cfafeffa2ceaeb685be856db1a2aed2a
laurendobratz00 <laurendobratz@icloud.com>
2021-05-19 19:29:59 +0000

    add file

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and prints the titles of the first
+10 hot posts listed for a given subreddit"""
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """ prints top ten titles """
+    url = "http://api.reddit.com/r/{}/hot?limit=10".format(subreddit)
+    headers = {
+        'User-Agent': 'nugget'
+    }
+    x = requests.get(url, headers=headers)
+    if not x:
+        print(None)
+    else:
+        data = x.json()
+        i = data.get('data').get('children')
+        for r in i:
+            s = r.get('data').get('title')
+            print(s)
